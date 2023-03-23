4a667ee4e67777d67ef7147f1e585724152ee674
Jay-Trent <jaypatel@trentu.ca>
2023-03-22 20:03:53 -0400

    init(set up completed)

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,30 @@
-// import { v4 as uuidV4 } from "uuid"
-console.log("Setup completed");
+let edaContainer = document.getElementById("edaTag");
+let predictionContainer = document.getElementById("predictionTag");
+let comparisionContainer = document.getElementById("comparisionTag");
+let listItems = document.getElementsByClassName("main_nav_ul_li");
 
 
-// type Task = {
-//   id: string
-//   title: string
-//   completed: boolean
-//   createdAt: Date
-// }
 
-// const list = document.querySelector<HTMLUListElement>("#list")
-// const form = document.getElementById("new-task-form") as HTMLFormElement | null
-// const input = document.querySelector<HTMLInputElement>("#new-task-title")
-// const tasks: Task[] = loadTasks()
-// tasks.forEach(addListItem)
 
-// form?.addEventListener("submit", e => {
-//   e.preventDefault()
-
-//   if (input?.value == "" || input?.value == null) return
-
-//   const newTask: Task = {
-//     id: uuidV4(),
-//     title: input.value,
-//     completed: false,
-//     createdAt: new Date(),
-//   }
-//   tasks.push(newTask)
-//   saveTasks()
-
-//   addListItem(newTask)
-//   input.value = ""
-// })
-
-// function addListItem(task: Task) {
-//   const item = document.createElement("li")
-//   const label = document.createElement("label")
-//   const checkbox = document.createElement("input")
-//   checkbox.addEventListener("change", () => {
-//     task.completed = checkbox.checked
-//     saveTasks()
-//   })
-//   checkbox.type = "checkbox"
-//   checkbox.checked = task.completed
-//   label.append(checkbox, task.title)
-//   item.append(label)
-//   list?.append(item)
-// }
-
-// function saveTasks() {
-//   localStorage.setItem("TASKS", JSON.stringify(tasks))
-// }
-
-// function loadTasks(): Task[] {
-//   const taskJSON = localStorage.getItem("TASKS")
-//   if (taskJSON == null) return []
-//   return JSON.parse(taskJSON)
-// }
+addRemoveSelection('edaTag')
+function addRemoveSelection(selectedView: string) {
+  console.log("--")
+  if (selectedView === "edaTag") {
+    comparisionContainer?.classList.add("RemoveFromView");
+    predictionContainer?.classList.add("RemoveFromView");
+    edaContainer?.classList.remove("RemoveFromView");
+    edaContainer?.classList.add("w3-animate-left");
+  } else if (selectedView == "predictionTag") {
+    comparisionContainer?.classList.add("RemoveFromView");
+    predictionContainer?.classList.add("RemoveFromView");
+    edaContainer?.classList.remove("RemoveFromView");
+    edaContainer?.classList.add("w3-animate-left")
+  } else if (selectedView == "comparisionTag") {
+    comparisionContainer?.classList.add("RemoveFromView");
+    predictionContainer?.classList.add("RemoveFromView");
+    edaContainer?.classList.remove("RemoveFromView");
+    edaContainer?.classList.add("w3-animate-left")
+  }
+}
+try {
+} catch (e) {}
