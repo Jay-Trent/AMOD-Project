6c92adb0761de0413e774ec28b7194becaf55806
Jay-Trent <jaypatel@trentu.ca>
2023-03-22 20:54:31 -0400

    nav bar completed

--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,56 @@ let predictionContainer = document.getElementById("predictionTag");
 let comparisionContainer = document.getElementById("comparisionTag");
 let listItems = document.getElementsByClassName("main_nav_ul_li");
 
-
-
+//Startup Binding
+listItems[0].getElementsByTagName("a")[0].classList.add("selectedOp");
 
 addRemoveSelection('edaTag')
+
+
+
+
+
+
+
+
+
+//Nav bar buttons change
+listItems[0].addEventListener("click", (e) => {
+  addRemoveSelection("edaTag");
+});
+listItems[1].addEventListener("click", (e) => {
+  addRemoveSelection("predictionTag");
+});
+listItems[2].addEventListener("click", (e) => {
+  addRemoveSelection("comparisionTag");
+});
+
+function removeSelection() {
+  for (let i = 0; i < listItems.length; i++) {
+    listItems[i].getElementsByTagName("a")[0].classList.remove("selectedOp");
+  }
+}
+
 function addRemoveSelection(selectedView: string) {
-  console.log("--")
+  removeSelection();
   if (selectedView === "edaTag") {
+    listItems[0].getElementsByTagName("a")[0].classList.add("selectedOp");
     comparisionContainer?.classList.add("RemoveFromView");
     predictionContainer?.classList.add("RemoveFromView");
     edaContainer?.classList.remove("RemoveFromView");
     edaContainer?.classList.add("w3-animate-left");
-  } else if (selectedView == "predictionTag") {
-    comparisionContainer?.classList.add("RemoveFromView");
-    predictionContainer?.classList.add("RemoveFromView");
-    edaContainer?.classList.remove("RemoveFromView");
-    edaContainer?.classList.add("w3-animate-left")
   } else if (selectedView == "comparisionTag") {
-    comparisionContainer?.classList.add("RemoveFromView");
+    listItems[2].getElementsByTagName("a")[0].classList.add("selectedOp");
+    edaContainer?.classList.add("RemoveFromView");
     predictionContainer?.classList.add("RemoveFromView");
-    edaContainer?.classList.remove("RemoveFromView");
-    edaContainer?.classList.add("w3-animate-left")
+    comparisionContainer?.classList.remove("RemoveFromView");
+    comparisionContainer?.classList.add("w3-animate-left");
+  } else if (selectedView == "predictionTag") {
+    listItems[1].getElementsByTagName("a")[0].classList.add("selectedOp");
+    comparisionContainer?.classList.add("RemoveFromView");
+    edaContainer?.classList.add("RemoveFromView");
+    predictionContainer?.classList.remove("RemoveFromView");
+    predictionContainer?.classList.add("w3-animate-left");
   }
 }
 try {
